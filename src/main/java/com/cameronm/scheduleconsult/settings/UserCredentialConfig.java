2420e2adb1efcf0f87e8a47f2728f2649de48989
Cameron <c-em@outlook.com>
2023-02-20 15:59:54 -0800

    Add UserCredentialConfig class for storing configuration settings related to credentials for accessing the database.

---
+++ src/main/java/com/cameronm/scheduleconsult/settings/UserCredentialConfig.java
@@ -0,0 +1,17 @@
+package com.cameronm.scheduleconsult.settings;
+
+/**
+ * The UserCredentialConfig class stores credential settings for accessing the server
+ */
+public class UserCredentialConfig {
+
+    /**
+     * The userName for admin access to the database
+     */
+    public static final String userName = "sqlUser";
+
+    /**
+     * The password for admin access to the database
+     */
+    public static String password = "Passw0rd!";
+}
