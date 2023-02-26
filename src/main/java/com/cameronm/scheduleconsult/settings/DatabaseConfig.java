9677b7e9d0ae1b94bbcea7fd53b323dcec07b4e6
Cameron <c-em@outlook.com>
2023-02-26 10:33:48 -0800

    Refactor variable names from camel case converted to snake case.

--- src/main/java/com/cameronm/scheduleconsult/settings/DatabaseConfig.java
+++ src/main/java/com/cameronm/scheduleconsult/settings/DatabaseConfig.java
@@ -11,30 +11,30 @@ public interface DatabaseConfig {
     /**
      * The location of the database
      */
-    String dbLocation = "//localhost/";
+    String DB_LOCATION = "//localhost/";
 
     /**
      * The name of the database
      */
-    String dbName = "client_schedule";
+    String DB_NAME = "client_schedule";
 
     /**
      * The protocol of the database
      */
-    String dbProtocol = "jdbc";
+    String DB_PROTOCOL = "jdbc";
 
     /**
      * The database vendor
      */
-    String dbVendor = ":mysql:";
+    String DB_VENDOR = ":mysql:";
 
     /**
      * The driver of the database
      */
-    String dbDriver = "com.mysql.cj.jdbc.Driver"; // version 8.0.25
+    String DB_DRIVER = "com.mysql.cj.jdbc.Driver"; // version 8.0.25
 
     /**
      * The timezone used when connecting to the database
      */
-    String dbTimezone = "?connectionTimeZone = SERVER";
+    String DB_TIMEZONE = "?connectionTimeZone = SERVER";
 }
